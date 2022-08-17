2fe0a91299692fee5bd7dd6f869550136539b8e3
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-08-17 23:24:42 +0200

    handlebars implemented but not style

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 
 const app = express();
 
 app.engine('.hbs',hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs')
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.post('/contact/send-message', upload.single('myFile'), (req, res) => {
+  const { author, sender, title, message, myFile} = req.body;
+  const fileName = req.file.originalname;
+  console.log('originalname: ', fileName)
+  if(author && sender && title && message && fileName) {
+    res.render('contact', { isSent : true, name1: fileName});
+  } else { res.render( 'contact' , { isError: true} )}
+});
 
 app.get( '/hello/:name' , (req, res) => {
   res.render('hello', { name: req.params.name})
@@ -22,11 +34,11 @@ app.get('/about', ( req,res ) => {
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact',{ layout: 'dark.hbs' });
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.render('info');
+  res.render('info', { layout: 'dark'});
 });
 
 app.get('/history', (req, res) => {
